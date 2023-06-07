45c28fecd8cfa3127bfbf17b053ab7e58225dae2
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-07 21:42:12 +0200

    chore: set swc minify to true

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  }
+}
 
 module.exports = nextConfig
