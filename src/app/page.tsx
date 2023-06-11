b5f4cfe7e03252a51c97b3e9fcd6797a6bd38edb
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-11 21:14:53 +0200

    feat: add brew ratio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,13 @@
 import HomeNavigation from "@/components/home-navigation";
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import Link from "next/link";
-import {Button} from "@/components/ui/button";
-
-const BeanconquerorCard = () => (
-    <Card className={"max-w-xl"}>
-        <CardHeader>
-            <CardTitle>Beanconqueror stats</CardTitle>
-            <CardDescription>
-                <Link
-                    className={"underline"}
-                    href={"https://beanconqueror.com/"}
-                    target={"_blank"}
-                >
-                    Beanconqueror
-                </Link> is an app for tracking coffee brews, both filter and espresso. This tool helps you visualise
-                your data
-            </CardDescription>
-        </CardHeader>
-        <CardContent className={"flex flex-col space-y-4"}>
-            <Link href={"/beanconqueror"} passHref>
-                <Button>Go to Beanconqueror stats</Button>
-            </Link>
-        </CardContent>
-    </Card>
-)
-
+import RatioCard from "@/components/ratio-card";
+import BeanconquerorCard from "@/components/bc-card";
 
 export default async function Home() {
       return (
         <main className="flex h-full flex-col items-center p-6 md:p-24 space-y-6">
             <HomeNavigation />
-            <BeanconquerorCard  />
+            <BeanconquerorCard />
+            <RatioCard />
         </main>
       )
 }
---
+++ src/components/ratio-card.tsx
@@ -0,0 +1,115 @@
+"use client"
+
+import {ChangeEvent, ReactNode, useState} from "react";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {InputWithPrefix} from "@/components/ui/input-with-prefix";
+
+interface UserInputProps {
+    id: string;
+    value: number | undefined;
+    label: string;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    prefix?: string;
+}
+
+const UserInputGroup = ({children}: {children: ReactNode}) => (
+    <div className={"flex gap-2 md:gap-4"}>
+        {children}
+    </div>
+)
+
+const UserInput = ({id, label, value, onChange, prefix}: UserInputProps) => (
+    <div className="grid w-1/2 max-w-sm items-center gap-1.5">
+        <Label htmlFor={id}>{label}</Label>
+        {!prefix && (<Input
+            id={id}
+            value={value}
+            type="number"
+            className={"text-lg"}
+            onChange={onChange}
+        />)}
+        {!!prefix && (
+            <InputWithPrefix
+                prefix={prefix}
+                id={id}
+                value={value}
+                type="number"
+                className={"text-lg"}
+                onChange={onChange}
+            />
+        )}
+    </div>
+)
+
+
+const CalculatedValue = ({title, value}: {title: string, value: string}) => (
+    <div className={"flex flex-col space-y-1.5 items-center"}>
+        <span className={"font-light"}>{title}</span>
+        <span className={"font-bold text-5xl"}>{value}</span>
+    </div>
+
+)
+
+
+const RatioCard = () => {
+    const [ratio, setRatio] = useState<number | undefined>(16);
+    const [coffee, setCoffee] = useState<number | undefined>(12.5);
+    const [water, setWater] = useState<number | undefined>(200);
+
+    const onChange = (setFunc: any) => (event:ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+
+        if (value === "") {
+            return setFunc(undefined);
+        }
+        setFunc(parseInt(value))
+    }
+
+    const onChangeWater = onChange(setWater);
+    const onChangeCoffee = onChange(setCoffee);
+    const onChangeRatio = onChange(setRatio);
+
+    return (
+        <Card className={"w-full max-w-xl"}>
+            <CardHeader>
+                <CardTitle>Brew ratio</CardTitle>
+                <CardDescription>Calculate your brew ratio</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Tabs defaultValue={"coffee"}>
+                    <TabsList>
+                        <TabsTrigger value={"coffee"}>Coffee</TabsTrigger>
+                        <TabsTrigger value={"water"}>Water</TabsTrigger>
+                        <TabsTrigger value={"ratio"}>Ratio</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value={"coffee"} className={"space-y-2 md:space-y-4"}>
+                        <UserInputGroup>
+                            <UserInput id={"coffee-tab_ratio"} label={"Ratio"} value={ratio} onChange={onChangeRatio} prefix={"1/"}/>
+                            <UserInput id={"coffee-tab_water"} label={"Water (gr/ml)"} value={water} onChange={onChangeWater} />
+                        </UserInputGroup>
+                        <CalculatedValue title={"Calculated ground coffee (gr/ml)"} value={!!ratio && !!water ? (water / ratio).toFixed(2) : "-"} />
+                    </TabsContent>
+                    <TabsContent value={"water"} className={"space-y-2 md:space-y-4"}>
+                        <UserInputGroup>
+                            <UserInput id={"water-tab_ratio"} label={"Ratio"} value={ratio} onChange={onChangeRatio} />
+                            <UserInput id={"water-tab_coffee"} label={"Ground coffee (gr)"} value={coffee} onChange={onChangeCoffee} />
+                        </UserInputGroup>
+                        <CalculatedValue title={"Calculated water (gr/ml)"} value={!!ratio && !!coffee ? (ratio * coffee).toFixed(2) : "-"} />
+                    </TabsContent>
+                    <TabsContent value={"ratio"} className={"space-y-2 md:space-y-4"}>
+                        <UserInputGroup>
+                            <UserInput id={"ratio-tab_water"} label={"Water (gr/ml("} value={water} onChange={onChangeWater} />
+                            <UserInput id={"ratio-tab_coffee"} label={"Ground coffee (gr)"} value={coffee} onChange={onChangeCoffee} />
+                        </UserInputGroup>
+                        <CalculatedValue title={"Calculated ratio"} value={!!water && !! coffee ? `1/${(water / coffee).toFixed(2)}` : "-"} />
+                    </TabsContent>
+                </Tabs>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default RatioCard
