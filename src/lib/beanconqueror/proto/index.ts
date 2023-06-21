20aa61fa3471786aa38118dd263e000b19e20bcb
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-21 14:51:11 +0200

    fix: replace spaces with plusses

--- src/lib/beanconqueror/proto/index.ts
+++ src/lib/beanconqueror/proto/index.ts
@@ -1,12 +1,10 @@
 import root from "@/lib/beanconqueror/proto/generated/beanconqueror";
 
-const decodeBase64 = (encoded: string) => Buffer.from(encoded, "base64").toString("latin1");
-
 const decodeUrl = (url: string) => {
     const params = (new URL(url)).searchParams;
 
     let i = 0;
-    let sharedBean = "";
+    let sharedBeans = "";
 
     while (true) {
         const param = params.get(`shareUserBean${i}`) || "";
@@ -15,15 +13,14 @@ const decodeUrl = (url: string) => {
             break;
         }
 
-        sharedBean += decodeBase64(param);
+        sharedBeans += decodeURIComponent(param);
         i++;
     }
-
-    return sharedBean
+    return atob(sharedBeans.replaceAll(" ", "+"))
 }
 
 export function decodeMessage(url: string) {
     const sharedBean = decodeUrl(url);
-    const buffer = Buffer.from(sharedBean, "latin1");
+    const buffer = new Uint8Array([...Array.from(sharedBean)].map((char) => char.charCodeAt(0)));
     return root.beanconqueror.BeanProto.decode(buffer);
 }
