7f71cd0441d90e3dd7cf3c71b1681bcf263f8341
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-21 18:23:05 +0200

    feat: add hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,17 @@ import {Button} from "@/components/ui/button";
 import {Github} from "lucide-react";
 import Link from "next/link";
 import CardGrid from "@/components/ui/card-grid";
+import Image from "next/image";
 
 export default async function Home() {
     return (
         <>
             <HomeNavigation/>
+            <div>
+                <Image className={"block dark:hidden"} width={500} height={500} src={"/beanstats_img_light.png"} alt={"An illustration of coffee"} />
+                <Image className={"hidden dark:block"} width={500} height={500} src={"/beanstats_img_dark.png"} alt={"An illustration of coffee"} />
+
+            </div>
             <CardGrid>
                 <BeanconquerorCard/>
                 <RatioCard/>
