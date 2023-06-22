8ad3b7f23bcd04e2b1f16ee8726773d0a5794f27
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-22 13:43:15 +0200

    chore: center hero text

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import {H1} from "@/components/text";
 const Hero = () => {
     return (
         <div className={"flex flex-col items-center w-full max-w-3xl space-y-6"}>
-            <H1>
+            <H1 className={"text-center"}>
                 Coffee tools and visualisation with&nbsp;
                 <span className={"gradient-text"}>Beanstats</span>
             </H1>
