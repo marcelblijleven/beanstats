b0d16723a3b5e1af22de25fb018493c46b5ce987
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-27 14:08:48 +0200

    chore: add form item wrapper

---
+++ src/components/forms/inputs/form-item-wrapper.tsx
@@ -0,0 +1,27 @@
+import {FormItem, FormControl, FormLabel, FormMessage, FormDescription} from "@/components/ui/form";
+import {ReactNode} from "react";
+import {cn} from "@/lib/utils";
+
+export interface FormItemProps {
+    label: string;
+    description?: string;
+    className?: string;
+    children: ReactNode
+}
+
+export function FormItemWrapper(props: FormItemProps) {
+    return (
+        <FormItem className={cn(props.className)}>
+            <FormLabel>{props.label}</FormLabel>
+            <FormControl>
+                {props.children}
+            </FormControl>
+            {!!props.description && (
+                <FormDescription>
+                    {props.description}
+                </FormDescription>
+            )}
+            <FormMessage/>
+        </FormItem>
+    )
+}
