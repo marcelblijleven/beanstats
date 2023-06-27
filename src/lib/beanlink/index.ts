7eaa824729157dbd0cfeae8abf6147bbe2051579
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-27 14:08:05 +0200

    chore: remove wait and log

--- src/lib/beanlink/index.ts
+++ src/lib/beanlink/index.ts
@@ -5,13 +5,7 @@ export interface BeanLinkResponse {
     roaster?: string | null;
 }
 
-const wait = (): Promise<string> => {
-    return new Promise(resolve => setTimeout(resolve, 3000))
-}
-
 export async function getBeanLink(link: string): Promise<BeanLinkResponse> {
-    console.log("bai")
-    await wait()
     const response = await fetch("https://beanl.ink/add", {
         method: "POST",
         body: JSON.stringify({"link": link})
