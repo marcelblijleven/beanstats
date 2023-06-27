9f25e75dad6097e55862d7faed07cbb3119cb6da
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-27 14:08:32 +0200

    chore: change import

--- src/app/beanconqueror/(share)/create/page.tsx
+++ src/app/beanconqueror/(share)/create/page.tsx
@@ -1,4 +1,4 @@
-import Form from "@/components/forms/bean-information-form";
+import Form from "@/components/forms/bean-information-form/form";
 import {Metadata} from "next";
 
 export const metadata: Metadata = {
