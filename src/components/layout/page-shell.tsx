ed0cfcef7f35fccba2dad381f9f1f7d1385dd9e4
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-29 18:00:38 +0200

    chore: add padding to page shell

--- src/components/layout/page-shell.tsx
+++ src/components/layout/page-shell.tsx
@@ -2,7 +2,7 @@ import {HTMLAttributes} from "react";
 
 export default function PageShell(props: HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className={"flex flex-col items-center w-full max-w-5xl space-y-4"}>
+        <div className={"flex flex-col items-center w-full max-w-5xl space-y-4 pb-6"}>
             {props.children}
         </div>
     )
