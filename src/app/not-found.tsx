1561415808d1ad9aa23e20ba75edad70d8479c51
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-29 14:09:37 +0200

    fix: add custom not found page for better ux

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import PageShell from "@/components/layout/page-shell";
+import {H1} from "@/components/text";
+import {AlertTriangle} from "lucide-react";
+
+export default function NotFound() {
+    return (
+        <PageShell>
+            <H1 className={"gradient-text"}>Not Found :(</H1>
+            <AlertTriangle className={"text-amber-700 h-12 w-12"} />
+            <p className={"font-medium"}>Could not find the page you&apos;re looking for..</p>
+            <p>
+                View <Link className={"underline text-amber-500"} href="/">home page</Link>
+            </p>
+        </PageShell>
+    )
+}
