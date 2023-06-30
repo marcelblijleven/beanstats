c57088f077192a7c08f766704ed2a3776876e890
Marcel Blijleven <marcelblijleven@gmail.com>
2023-06-30 09:51:42 +0200

    feat: notify users on upload error

--- src/components/beanconqueror/stats/StatsContainer.tsx
+++ src/components/beanconqueror/stats/StatsContainer.tsx
@@ -6,6 +6,8 @@ import {processBCFile, Statistics as BrewStatistics} from "@/lib/beanconqueror/s
 import {Button} from "@/components/ui/button";
 import Statistics from "@/app/beanconqueror/stats/components/statistics";
 import {useState} from "react";
+import {AlertCircle} from "lucide-react";
+import {AlertRoot as Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
 
 export function StatsContainer() {
     const [data, setData] = useState<BrewStatistics>();
@@ -16,7 +18,17 @@ export function StatsContainer() {
 
     return (
         <>
-            {!data && <FileUpload callback={(contents) => processBCFile(contents, retrieveData)}/>}
+            <div className={"space-y-4"}>
+                {!data && <FileUpload callback={(contents) => processBCFile(contents, retrieveData)}/>}
+                <Alert>
+                    <AlertCircle className="h-4 w-4" />
+                    <AlertTitle>Heads up!</AlertTitle>
+                    <AlertDescription>
+                        The new Beanconqueror zip file is not working in some cases. A fix will be out soon
+                    </AlertDescription>
+                </Alert>
+
+            </div>
             {!!data && (
                 <>
                     <Button onClick={() => setData(undefined)}>Upload another file</Button>
