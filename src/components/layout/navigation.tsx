ca5091a4006d2a610ebb4118a375df3605a66ec8
Marcel Blijleven <marcelblijleven@gmail.com>
2023-09-18 22:36:46 +0200

    chore: add dashboard to navigation

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -1,10 +1,36 @@
-import {NavigationMenu, NavigationMenuList} from "@/components/ui/navigation-menu";
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList, navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu";
 import UserButtonMenuItem from "@/components/user/user-button";
+import Link from "next/link";
+
+type MenuItemProps = {
+    href: string,
+    label: string,
+}
+
+
+function MenuItem(props: MenuItemProps) {
+    return (
+        <NavigationMenuItem>
+            <Link href={props.href} legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                {props.label}
+            </NavigationMenuLink>
+        </Link>
+        </NavigationMenuItem>
+    )
+}
+
 
 export function Navigation() {
     return (
         <NavigationMenu>
             <NavigationMenuList>
+                <MenuItem href={"/dashboard"} label={"Dashboard"} />
                 <UserButtonMenuItem />
             </NavigationMenuList>
         </NavigationMenu>
