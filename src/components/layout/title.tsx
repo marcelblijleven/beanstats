9f095a78ddd869d3a2aed4e279c5701466f52d46
Marcel Blijleven <marcelblijleven@gmail.com>
2023-09-25 13:59:37 +0200

    chore: layout improvements in title

--- src/components/layout/title.tsx
+++ src/components/layout/title.tsx
@@ -1,7 +1,7 @@
 export function Title({title, subtitle}: {title: string, subtitle: string}) {
     return (
-        <section className={"space-y-2"}>
-            <h1 className={"text-5xl font-bold"}>{title}</h1>
+        <section className={"space-y-0.5"}>
+            <h1 className={"text-3xl md:text-5xl font-bold tracking-tight"}>{title}</h1>
             <h2 className={"max-w-[540px] text-lg text-muted-foreground"}>{subtitle}</h2>
         </section>
     )
