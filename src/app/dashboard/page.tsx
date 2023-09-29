777e43331c571f441a0c1d080c87d5baaddc1c52
Marcel Blijleven <marcelblijleven@gmail.com>
2023-09-29 14:07:16 +0200

    imp: improve buttons on dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,18 @@
-import {Button} from "@/components/ui/button";
+import {Button, buttonVariants} from "@/components/ui/button";
 import {StatsCardSection} from "@/app/dashboard/components/stats-card-section";
+import Link from "next/link";
+import {cn} from "@/lib/utils";
 
 
 export default async function DashboardPage() {
     return (
         <>
             <section>
-                <div className={"space-x-2"}>
-                    <Button className={"hidden md:inline-block"} disabled>Add coffee</Button>
+                <div className={"justify-between items-center space-x-2"}>
+                    <Link href={"/coffee/add"} className={cn(buttonVariants({variant: "default", size: "default"}), "hidden md:inline-flex")}>Add coffee</Link>
                     <Button className={"hidden md:inline-block"} disabled>Add brew</Button>
                     <Button className={"hidden md:inline-block"} disabled>Add cafe brew</Button>
-                    <Button size={"sm"} className={"inline-block md:hidden"} disabled>Add coffee</Button>
+                    <Link href={"/coffee/add"} className={cn(buttonVariants({variant: "default", size: "sm"}), "inline-flex md:hidden")}>Add coffee</Link>
                     <Button size={"sm"} className={"inline-block md:hidden"} disabled>Add brew</Button>
                     <Button size={"sm"} className={"inline-block md:hidden"} disabled>Add cafe brew</Button>
                 </div>
