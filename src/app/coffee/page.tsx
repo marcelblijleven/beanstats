d02e8bf557de406bfd85c0d651bf503f8ea6df80
Marcel Blijleven <marcelblijleven@gmail.com>
2023-09-29 14:06:05 +0200

    chore: add placeholder for coffee page

---
+++ src/app/coffee/page.tsx
@@ -0,0 +1,15 @@
+import {Title} from "@/components/layout/title";
+
+export default function CoffeePage() {
+    return (
+        <>
+            <Title
+                title={"Coffee"}
+                subtitle={"Track your coffee backlog"}
+            />
+            <section>
+                <h3>More to come soon</h3>
+            </section>
+        </>
+    )
+}
