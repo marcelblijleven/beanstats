7e6ba957cf2b3d781f2d4b1c4e6a61a826247f91
Marcel Blijleven <marcelblijleven@gmail.com>
2023-09-29 14:05:12 +0200

    chore: add useEditCheck

---
+++ src/app/coffee/components/hooks.ts
@@ -0,0 +1,12 @@
+import {usePathname} from "next/navigation";
+
+export const useEditCheck = () => {
+    const pathname = usePathname();
+    const isEdit = pathname.includes("edit")
+
+    return {
+        isEdit,
+        currentPath: pathname,
+        detailPage: isEdit ? pathname.replace("/edit", "") : null,
+    }
+}
