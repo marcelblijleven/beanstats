c5f9a59699db14a507a6c82cced287c8785427ff
Marcel Blijleven <marcelblijleven@gmail.com>
2023-10-23 14:14:23 +0200

    feat: add favorites to dashboard

--- src/app/dashboard/components/stats-card-section.tsx
+++ src/app/dashboard/components/stats-card-section.tsx
@@ -3,16 +3,14 @@ import {type ReactNode} from "react";
 
 import {getMetrics} from "@/lib/db/get-metrics";
 
-import { StatsCard, StatsCardSkeleton } from "./stats-card";
+import {FavoriteStatsCard, FavoriteStatsCardSkeleton, StatsCard, StatsCardSkeleton} from "./stats-card";
 
 
-function Layout({children}: {children: ReactNode}) {
+function Layout({children}: { children: ReactNode }) {
     return (
         <section className={"mt-12"}>
             <h2 className={"text-xl font-bold"}>Stats</h2>
-            <div className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4"}>
-                {children}
-            </div>
+            {children}
         </section>
     )
 }
@@ -31,34 +29,56 @@ export async function StatsCardSection() {
 
     return (
         <Layout>
-            <StatsCard
-                title={"Coffee"}
-                subtitle={"Bags of coffee"}
-                value={metrics.beansCount}
-                href={"/coffee"}
-            />
-            <StatsCard
-                title={"Freezer"}
-                subtitle={"Frozen entries"}
-                value={metrics.freezerCount}
-                href={"/coffee/freeze"}
-            />
-            <StatsCard
-                title={"Roasters"}
-                subtitle={"Unique roasters"}
-                value={metrics.roasterCount}
-            />
-            <StatsCard
-                title={"Cafe brews"}
-                subtitle={"cafe brews logged"}
-                value={metrics.cafeBrewsCount}
-                href={"/brews/cafe"}
-            />
-            <StatsCard
-                title={"Brews"}
-                subtitle={"brews logged"}
-                value={"Coming soon"}
-            />
+            <div className={"space-y-2 md:space-y-4"}>
+                <div className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4"}>
+                    <StatsCard
+                        title={"Coffee"}
+                        subtitle={"Bags of coffee"}
+                        value={metrics.beansCount}
+                        href={"/coffee"}
+                    />
+                    <StatsCard
+                        title={"Freezer"}
+                        subtitle={"Frozen entries"}
+                        value={metrics.freezerCount}
+                        href={"/coffee/freeze"}
+                    />
+                    <StatsCard
+                        title={"Roasters"}
+                        subtitle={"Unique roasters"}
+                        value={metrics.roasterCount}
+                    />
+                    <StatsCard
+                        title={"Cafe brews"}
+                        subtitle={"cafe brews logged"}
+                        value={metrics.cafeBrewsCount}
+                        href={"/brews/cafe"}
+                    />
+                    <StatsCard
+                        title={"Brews"}
+                        subtitle={"brews logged"}
+                        value={"Coming soon"}
+                    />
+                </div>
+                <div className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4"}>
+                    <FavoriteStatsCard title={"Roasters"}
+                                       subtitle={"By bags"}
+                                       values={metrics.favoriteRoastersByBag}
+                    />
+                    <FavoriteStatsCard title={"Roasters"}
+                                       subtitle={"By weight"}
+                                       values={metrics.favoriteRoastersByWeight}
+                    />
+                    <FavoriteStatsCard title={"Varieties"}
+                                       subtitle={"By occurrence"}
+                                       values={metrics.favoriteVarieties}
+                    />
+                    <FavoriteStatsCard title={"Origins"}
+                                       subtitle={"By occurrence"}
+                                       values={metrics.favoriteOrigins}
+                    />
+                </div>
+            </div>
         </Layout>
     )
 }
@@ -66,11 +86,21 @@ export async function StatsCardSection() {
 export function StatsCardSectionSkeleton() {
     return (
         <Layout>
-            <StatsCardSkeleton />
-            <StatsCardSkeleton />
-            <StatsCardSkeleton />
-            <StatsCardSkeleton />
-            <StatsCardSkeleton />
+            <div className={"space-y-2 md:space-y-4"}>
+                <div className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4"}>
+                    <StatsCardSkeleton/>
+                    <StatsCardSkeleton/>
+                    <StatsCardSkeleton/>
+                    <StatsCardSkeleton/>
+                    <StatsCardSkeleton/>
+                </div>
+                <div className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4"}>
+                    <FavoriteStatsCardSkeleton />
+                    <FavoriteStatsCardSkeleton />
+                    <FavoriteStatsCardSkeleton />
+                    <FavoriteStatsCardSkeleton />
+                </div>
+            </div>
         </Layout>
     )
 }
