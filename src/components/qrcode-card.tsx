ab15da83e708efb79f349d4cedfcfafaaa65be56
Marcel Blijleven <marcelblijleven@gmail.com>
2023-12-31 21:46:15 +0100

    refactor: allow for displaying qrcode without card

--- src/components/qrcode-card.tsx
+++ src/components/qrcode-card.tsx
@@ -1,8 +1,19 @@
-import { QRCode } from "react-qrcode-logo";
+import { QRCode as QRCodeComponent } from "react-qrcode-logo";
 
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 
 const QRCodeSize = 200;
+
+export const QRCode = ({value}: {value: string}) => (
+  <QRCodeComponent
+    value={value}
+    size={QRCodeSize}
+    logoImage={value.length > 40 ? "" : "/beanconqueror_logo.png"}
+    logoHeight={0.2 * QRCodeSize}
+    logoWidth={0.2 * QRCodeSize}
+    removeQrCodeBehindLogo={true}
+  />
+)
 
 const QRCodeCard = ({value}: {value: string}) => (
     <Card className={"bg-white"} style={{color: "hsl(222.2 47.4% 11.2%)"}}>
@@ -11,14 +22,7 @@ const QRCodeCard = ({value}: {value: string}) => (
             <CardDescription>Scan this with your camera app, not the Beanconqueror app</CardDescription>
         </CardHeader>
         <CardContent className={"flex justify-center"}>
-            <QRCode
-                value={value}
-                size={QRCodeSize}
-                logoImage={value.length > 40 ? "" : "/beanconqueror_logo.png"}
-                logoHeight={0.2 * QRCodeSize}
-                logoWidth={0.2 * QRCodeSize}
-                removeQrCodeBehindLogo={true}
-            />
+            <QRCode value={value} />
         </CardContent>
     </Card>
 );
