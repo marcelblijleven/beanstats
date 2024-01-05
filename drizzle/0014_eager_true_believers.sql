2b811d37b2d10c88d2c50d005a3925fcad809459
Marcel Blijleven <marcelblijleven@gmail.com>
2024-01-05 23:15:19 +0100

    fix: drop index instead of constraint

--- drizzle/0014_eager_true_believers.sql
+++ drizzle/0014_eager_true_believers.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `roasters` DROP CONSTRAINT `roasters_name_unique`;--> statement-breakpoint
+ALTER TABLE `roasters` DROP INDEX `roasters_name_unique`;--> statement-breakpoint
 ALTER TABLE `roasters` ADD CONSTRAINT `roasters_user_id_name_unique` UNIQUE(`user_id`,`name`);
