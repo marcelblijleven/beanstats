a401e3b0837d1ad1af07430e059cad62662cce5a
Marcel Blijleven <marcelblijleven@gmail.com>
2024-01-07 22:29:11 +0100

    style: remove page shell and make heading the same as home page

--- src/app/beanconqueror/(share)/shorten/page.tsx
+++ src/app/beanconqueror/(share)/shorten/page.tsx
@@ -1,7 +1,6 @@
 import {type Metadata} from "next";
 
 import {ShortenContainer} from "@/components/beanconqueror/share/shorten/shorten-link-container";
-import PageShell from "@/components/layout/page-shell";
 
 export const metadata: Metadata = {
     title: "Shorten Beanconqueror link",
@@ -15,14 +14,19 @@ export const metadata: Metadata = {
 
 export default function CreateShareLinkPage() {
     return (
-        <PageShell>
-            <h1 className={"text-4xl md:text-6xl font-bold text-center"}>
-                Shorten a <span className={"gradient-text"}>Beanconqueror</span> share link
-            </h1>
-            <p className={"text-center"}>
-                This form uses Beanlink to shorten a Beanconqueror share link.
-            </p>
-            <ShortenContainer link={null}  />
-        </PageShell>
+      <div className={"flex flex-col items-center"}>
+        <section className={"text-center max-w-xl space-y-6"}>
+          <h1 className={"text-4xl md:text-6xl font-bold text-center"}>
+            Shorten a <span className={"gradient-text"}>Beanconqueror</span> share link
+          </h1>
+          <p className={"text-center"}>
+            This form uses Beanlink to shorten a Beanconqueror share link.
+          </p>
+        </section>
+        <div className={"w-full my-6"}>
+          <ShortenContainer link={null}  />
+        </div>
+      </div>
+
     );
 }
--- src/app/beanconqueror/stats/page.tsx
+++ src/app/beanconqueror/stats/page.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import {StatsContainer} from "@/components/beanconqueror/stats/StatsContainer";
-import PageShell from "@/components/layout/page-shell";
 
 export default function BeanconquerorPage() {
     return (
-        <PageShell>
+        <div className={"flex flex-col items-center"}>
+          <section className={"text-center max-w-xl space-y-6"}>
             <h1 className={"text-4xl md:text-6xl font-bold text-center"}>
-                <span className={"gradient-text"}>Upload</span> a Beanconqueror file to view your data
+              <span className={"gradient-text"}>Upload</span> a Beanconqueror file to view your data
             </h1>
             <p className={"text-lg text-center max-w-2xl"}>
                 Use the select file button below and search for your Beanconqueror.json file.
             </p>
-            <StatsContainer />
-        </PageShell>
+          </section>
+          <StatsContainer />
+        </div>
     );
 }
